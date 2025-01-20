8e9f2bcfe3980f59311ab76508957d927b69a8b5
SangHamChoi <sangdnse184169@fpt.edu.vn>
2025-01-20 14:40:29 +0700

    Added custom collection models

---
+++ BE/Model/Model/Custom_Collection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Model
+{
+    public class CustomCollection
+    {
+        public string BodyHtml { get; set; }
+        public string Handle { get; set; }
+      
+        public Int64 Id { get; set; }
+        public bool Published { get; set; }
+        public DateTime PublishedAt { get; set; }
+        public string PublishedScope { get; set; }
+      
+        public string Title { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int ProductsCount { get; set; }
+    }
+}
