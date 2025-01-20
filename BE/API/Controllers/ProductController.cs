4bb20cf944c8e61bf0c97c2a08ac120d62d309c4
SangHamChoi <sangdnse184169@fpt.edu.vn>
2025-01-20 13:37:38 +0700

    get product id get product limit

--- BE/API/Controllers/ProductController.cs
+++ BE/API/Controllers/ProductController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/product")]
 [ApiController]
 public class ProductController : ControllerBase {
     private string apiKey = Helper.Configuration.GetConfiguration()["Others:HaravanToken"];
@@ -20,19 +20,60 @@ public class ProductController : ControllerBase {
             return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
         }
     }
-
-    [HttpGet("limit")]
-    public async Task<ActionResult> GetLimit(int amount){
-        try{
-            HttpResponseMessage result = await Helper.Client.GetAsync("products.json");
-            if( result == null )
-                return StatusCode((int)result.StatusCode, result.Content.ReadAsStringAsync());
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetById(int id)
+    {
+        try
+        {
+            string url = $"products/{id}.json";
+            HttpResponseMessage result = await Helper.Client.GetAsync(url);
+            if (result == null || !result.IsSuccessStatusCode)
+            {
+                return StatusCode((int)result.StatusCode, await result.Content.ReadAsStringAsync());
+            }
             return StatusCode(StatusCodes.Status200OK, await result.Content.ReadAsStringAsync());
-        } catch ( Exception ex ) {
+        }
+        catch (Exception ex)
+        {
+          
             Console.WriteLine(ex);
             return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
         }
     }
+
+    [HttpGet("limit")]
+    public async Task<ActionResult> GetLimit(int page = 1, int limit = 50)
+    {
+        try
+        {
+            int haravanPageLimit = 50;
+            int offset = (page - 1) * limit;
+            int haravanPage = offset / haravanPageLimit + 1;
+            int haravanLimit = limit;
+
+            if (offset % haravanPageLimit != 0)
+            {
+                haravanLimit = haravanPageLimit - (offset % haravanPageLimit);
+            }
+
+            string url = $"products.json?limit={haravanLimit}&page={haravanPage}";
+
+            HttpResponseMessage result = await Helper.Client.GetAsync(url);
+
+            if (result == null || !result.IsSuccessStatusCode)
+            {
+                return StatusCode((int)result.StatusCode, await result.Content.ReadAsStringAsync());
+            }
+
+            return StatusCode(StatusCodes.Status200OK, await result.Content.ReadAsStringAsync());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
 
 }
 
