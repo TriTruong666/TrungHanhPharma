a7bb9f6348c6e641f9eb5aab97d37b64ec8a6861
TriTruong666 <tritruonghoang3@gmail.com>
2025-01-21 11:50:25 +0700

    add BE to tritruong

--- BE/Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
+++ BE/Model/obj/Debug/net8.0/Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d29418fde0620a46ed1e05d8a6be667b3a733f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+372026472d5261a32b27c31933e5e6920e6a4cbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
