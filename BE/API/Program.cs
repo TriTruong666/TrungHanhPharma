372026472d5261a32b27c31933e5e6920e6a4cbb
TriTruong666 <tritruonghoang3@gmail.com>
2025-01-21 11:35:05 +0700

    Stable FE and BE

---
+++ BE/API/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers().AddJsonOptions( options => {
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAllOrigins");
+app.MapControllers();
+
+app.Run();
---
+++ BE/Model/Model/Order.cs
@@ -0,0 +1,54 @@
+﻿#nullable disable
+
+namespace Models
+{
+    // DiscountCode model (assuming you have this already)
+    public class DiscountCode
+    {
+        public string Code { get; set; }
+        public double Amount { get; set; }
+    }
+
+    // ItemInCart model
+    public class ItemInCart
+    {
+        public Int64 VariantId { get; set; }
+        public int Quantity { get; set; }
+        public string Sku { get; set; }
+        public string Name { get; set; }
+    }
+
+    // NoteAttribute model
+    public class NoteAttribute
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+    // ShippingAddress model
+    public class ShippingAddress
+    {
+        public string Address1 { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public string CountryCode { get; set; }
+        public string Province { get; set; }
+        public string ProvinceCode { get; set; }
+        public string DistrictCode { get; set; }
+        public string District { get; set; }
+        public string WardCode { get; set; }
+        public string Ward { get; set; }
+    }
+
+    // Order model
+    public class Order
+    {
+        public List<ItemInCart> LineItems { get; set; }
+        public double TotalDiscounts { get; set; }
+        public ShippingAddress ShippingAddress { get; set; }
+        public List<DiscountCode> DiscountCodes { get; set; }
+        public string Gateway { get; set; }
+    }
+}
